4a90c166718d8a7212dec2cd4f4eadc2dce1d8a1
varsha1226 <boyapativarsha26@gmail.com>
2025-02-10 11:36:56 +0530

    username and password

---
+++ Demo005.java
@@ -0,0 +1,12 @@
+
+public class Demo005 {
+    public static void main(String[] args) {
+        String Username = "user001";
+        String Password = "user001";
+        while ((Username == "user001") && (Password == "user001")) {
+            System.out.println("Hello AJAY Happy New Year");
+            break;
+        }
+    }
+
+}
