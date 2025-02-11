69784f78e33a8ff5b3389219862f072db8c60fb8
varsha1226 <boyapativarsha26@gmail.com>
2025-02-11 11:39:03 +0530

    while loop

---
+++ Demo006.java
@@ -0,0 +1,12 @@
+
+public class Demo006 {
+    public static void main(String[] args) {
+        String Username = "Ajay";
+        String Password = "Ajay";
+        while ((Username == "Ajay") && (Password == "Ajay")) {
+            System.out.println("Hello AJAY Happy New Year");
+            break;
+        }
+    }
+
+}
